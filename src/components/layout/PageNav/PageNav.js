de2116e9e18eacfade2324d2a9b60e2e3e26b8bd
Konrad <konrad.bonder@gmail.com>
2019-12-13 11:51:51 +0100

    Fix bug

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-import { PUBLIC_URL } from './settings';
+import { PUBLIC_URL } from '../../../settings';
 
 import styles from './PageNav.scss';
 
